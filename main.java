93a21675a1c2a1e95bdb69096305726b4506f44c
buzaldrin <114191903+buzaldrin@users.noreply.github.com>
2023-07-19 01:21:34 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,32 +1,58 @@
 import java.util.Scanner;
+
 public class Main {
+     private static Scanner sc =new Scanner(System.in);
     public static void main(String[] args) {
-       Edificio ed=new Edificio();
-       Scanner sc =new Scanner(System.in);
-       int op;
+
+
+        Edificio ed=new Edificio();
+
+        int op;
         System.out.println("bienvenido a brisa alta");
-    final String RESET = "\033[0m";
-       do {
-           System.out.println(RESET);
-           System.out.println("""
+        final String RESET = "\033[0m";
+        do {
+            System.out.println(RESET);
+            System.out.println("""
                    1-ver edificio
                    2-comprar
                    3-buscar dueño
                    4-ver ganancias
-                   5-salir
+                   5-eliminar dueño
+                   6-salir
                    """);
-           op=ed.verificarcant(0,6," una opcion ");
-           switch (op) {
-               case 1 -> ed.ver_edificio();
-               case 2 -> ed.compra();
-               case 3 -> ed.buscar_dueno();
-               case 4 -> System.out.println("la ganancias son "+ed.ganacias);
-               case 5 -> System.out.println("adios");
-               default -> System.out.println("ingrese una opcion valida");
-           }
+            op=verificarcant(0,7," una opcion ");
+            switch (op) {
+                case 1 -> ed.ver_edificio();
+                case 2 -> ed.compra();
+                case 3 -> ed.buscar_dueno();
+                case 4 -> System.out.println("la ganancias son "+ed.verGanancias());
+                case 5-> ed.eliminarDueno();
+                case 6 -> System.out.println("adios");
+                default -> System.out.println("ingrese una opcion valida");
+            }
 
-       }while (op!=5);
+        }while (op!=6);
 
     }
 
-}
+    public static int verificarcant(int minimo, long  maximo, String Stesto){
+        while (true) {
+            try {
+                System.out.println("ingrese "+Stesto);
+                while (!sc.hasNextInt()) {
+                    System.out.println("ingrese un numero entero ");
+                    sc.next();
+                }
+                int numero = sc.nextInt();
+                if (numero>minimo && numero<maximo){
+                    return numero;
+                }else {
+                    System.out.println("debe ser entre "+(minimo+1)+" y "+(maximo-1));
+                }
+            } catch (Exception e) {
+                System.out.println("ingrese el rut sin digito verificador ni guion");
+            }
+        }
+    }
+
+    }
